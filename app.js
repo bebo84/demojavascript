d9b17ae15ec9e468df4d009fa8ccbebfee3bab00
bebo84 <congtacbaogmail.com>
2015-10-11 20:33:08 +0700

    change code for case: onlt one word1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Created by PHUONG on 10/10/2015.
  */
 
-SuperString = require('./SuperString.js');
+SuperString = require('./SuperString/SuperString.js');
 
 console.log(SuperString.camelcase("hello how are you"));
 console.log(SuperString.longestWord(""));
